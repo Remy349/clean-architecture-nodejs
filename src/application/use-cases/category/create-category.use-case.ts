4934896b6c4e061169bc625cd762c1644d46d34b
Remy349 <santoraga15@gmail.com>
2024-12-06 13:57:42 -0600

    feat: Add types for use cases

--- src/application/use-cases/category/create-category.use-case.ts
+++ src/application/use-cases/category/create-category.use-case.ts
@@ -1,6 +1,8 @@
 import { ICategoryRepository } from "@/application/repositories/category.repository.interface";
 import { Category, CreateCategory } from "@/entities/models/category.model";
 
+export type ICreateCategoryUseCase = ReturnType<typeof createCategoryUseCase>;
+
 export const createCategoryUseCase =
   (categoryRepository: ICategoryRepository) =>
   async (data: CreateCategory): Promise<Category> => {
--- src/application/use-cases/category/get-all-categories.use-case.ts
+++ src/application/use-cases/category/get-all-categories.use-case.ts
@@ -1,6 +1,10 @@
 import { ICategoryRepository } from "@/application/repositories/category.repository.interface";
 import { Category } from "@/entities/models/category.model";
 
+export type IGetAllCategoriesUseCase = ReturnType<
+  typeof getAllCategoriesUseCase
+>;
+
 export const getAllCategoriesUseCase =
   (categoryRepository: ICategoryRepository) =>
   async (): Promise<Category[]> => {
