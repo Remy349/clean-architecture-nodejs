f9cbb5c075861543a0be5ff9c88b4f23b00ae19b
Remy349 <santoraga15@gmail.com>
2024-12-06 12:07:25 -0600

    refactor: Use factory pattern and convert classes use cases into functions

--- src/application/use-cases/category/create-category.use-case.ts
+++ src/application/use-cases/category/create-category.use-case.ts
@@ -1,10 +1,8 @@
 import { ICategoryRepository } from "@/application/repositories/category.repository.interface";
 import { Category, CreateCategory } from "@/entities/models/category.model";
 
-export class CreateCategoryUseCase {
-  constructor(private categoryRepository: ICategoryRepository) {}
-
-  async run(data: CreateCategory): Promise<Category> {
-    return await this.categoryRepository.create(data);
-  }
-}
+export const createCategoryUseCase =
+  (categoryRepository: ICategoryRepository) =>
+  async (data: CreateCategory): Promise<Category> => {
+    return await categoryRepository.create(data);
+  };
--- src/application/use-cases/category/get-all-categories.use-case.ts
+++ src/application/use-cases/category/get-all-categories.use-case.ts
@@ -1,10 +1,8 @@
 import { ICategoryRepository } from "@/application/repositories/category.repository.interface";
 import { Category } from "@/entities/models/category.model";
 
-export class GetAllCategoriesUseCase {
-  constructor(private categoryRepository: ICategoryRepository) {}
-
-  async run(): Promise<Category[]> {
-    return await this.categoryRepository.getAll();
-  }
-}
+export const getAllCategoriesUseCase =
+  (categoryRepository: ICategoryRepository) =>
+  async (): Promise<Category[]> => {
+    return await categoryRepository.getAll();
+  };
