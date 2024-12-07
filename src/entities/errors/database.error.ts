7e8edb539ad16e3e44aac700feecc07a835be9ec
Remy349 <santoraga15@gmail.com>
2024-12-07 00:52:45 -0600

    feat: Not found custom error added

--- src/entities/errors/database.error.ts
+++ src/entities/errors/database.error.ts
@@ -1,5 +1,11 @@
 import { ApiError } from "./api.error";
 
+export class DBNotFoundError extends ApiError {
+  constructor(message: string, options?: ErrorOptions) {
+    super(message, 404, options);
+  }
+}
+
 export class DBConflictError extends ApiError {
   constructor(message: string, options?: ErrorOptions) {
     super(message, 409, options);
