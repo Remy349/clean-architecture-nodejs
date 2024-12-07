6e5134b91a9eca1bd3d5bb98081de6ddbb3a30b7
Remy349 <santoraga15@gmail.com>
2024-12-07 00:53:52 -0600

    feat: Add new methods for delete and get by id category

--- src/application/repositories/category.repository.interface.ts
+++ src/application/repositories/category.repository.interface.ts
@@ -2,5 +2,7 @@ import { Category, CreateCategory } from "@/entities/models/category.model";
 
 export interface ICategoryRepository {
   getAll(): Promise<Category[]>;
+  getById(categoryId: number): Promise<Category | null>;
   create(data: CreateCategory): Promise<Category>;
+  delete(categoryId: number): Promise<void>;
 }
