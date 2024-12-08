66156b928a591b12e5264689f12bb912b251d1cc
Remy349 <santoraga15@gmail.com>
2024-12-08 00:47:15 -0600

    feat: New controllers added for user
    get all and get by id

---
+++ src/interface-adapters/controllers/user/get-all-users.controller.ts
@@ -0,0 +1,22 @@
+import { IGetAllUsersUseCase } from "@/application/use-cases/user/get-all-users.use-case";
+import { User } from "@/entities/models/user.model";
+
+const presenter = (users: User[]) => {
+  return users.map((user) => ({
+    id: user.id,
+    username: user.username,
+    password: user.password,
+    role: user.role,
+    createdAt: user.createdAt,
+  }));
+};
+
+export type IGetAllUsersController = ReturnType<typeof getAllUsersController>;
+
+export const getAllUsersController =
+  (getAllUsersUseCase: IGetAllUsersUseCase) =>
+  async (): Promise<ReturnType<typeof presenter>> => {
+    const users = await getAllUsersUseCase();
+
+    return presenter(users);
+  };
---
+++ src/interface-adapters/controllers/user/get-user-by-id.controller.ts
@@ -0,0 +1,24 @@
+import { IGetUserByIdUseCase } from "@/application/use-cases/user/get-user-by-id.use-case";
+import { User } from "@/entities/models/user.model";
+
+const presenter = (user: User | null) => {
+  return user
+    ? {
+        id: user.id,
+        username: user.username,
+        password: user.password,
+        role: user.role,
+        createdAt: user.createdAt,
+      }
+    : null;
+};
+
+export type IGetUserByIdController = ReturnType<typeof getUserByIdController>;
+
+export const getUserByIdController =
+  (getUserByIdUseCase: IGetUserByIdUseCase) =>
+  async (userId: number): Promise<ReturnType<typeof presenter>> => {
+    const user = await getUserByIdUseCase(userId);
+
+    return presenter(user);
+  };
