169b8212d5d31388aa64474b06a50b039ae6953e
Remy349 <santoraga15@gmail.com>
2024-12-09 12:54:44 -0600

    feat: Add auth di support

--- src/di/container.ts
+++ src/di/container.ts
@@ -2,11 +2,13 @@ import { createContainer } from "@evyweb/ioctopus";
 import { createCategoryModule } from "./modules/category.module";
 import { DI_RETURN_TYPES, DI_SYMBOLS } from "./types";
 import { createUserModule } from "./modules/user.module";
+import { createAuthModule } from "./modules/auth.module";
 
 const container = createContainer();
 
 container.load(Symbol("UserModule"), createUserModule());
 container.load(Symbol("CategoryModule"), createCategoryModule());
+container.load(Symbol("AuthModule"), createAuthModule());
 
 export function getInjection<
   K extends keyof DI_RETURN_TYPES,
--- src/di/types.ts
+++ src/di/types.ts
@@ -3,13 +3,16 @@ import {
   DI_CATEGORY_RETURN_TYPES,
   DI_CATEGORY_SYMBOLS,
 } from "./types/category.type";
+import { DI_AUTH_RETURN_TYPES, DI_AUTH_SYMBOLS } from "./types/auth.type";
 
 export const DI_SYMBOLS = {
   users: DI_USER_SYMBOLS,
   categories: DI_CATEGORY_SYMBOLS,
+  auth: DI_AUTH_SYMBOLS,
 };
 
 export type DI_RETURN_TYPES = {
   users: DI_USER_RETURN_TYPES;
   categories: DI_CATEGORY_RETURN_TYPES;
+  auth: DI_AUTH_RETURN_TYPES;
 };
